a62055c87ae9d9e4b6304cfb8ac9ed08c34eb546
Maximilian Oesterreicher <maxoeschi@outlook.de>
2021-09-28 11:18:43 +0200

    deleted performance stuff

--- library/relaxationRate/calculateCorrelationFunction.m
+++ library/relaxationRate/calculateCorrelationFunction.m
@@ -5,13 +5,10 @@ function [correlationFunction] = ...
 zeroPaddingLength = 2^(nextpow2(timeSteps)+1);
 
 % Convolution
-logMemoryUsage('C:\Users\maxoe\Google Drive\Promotion\Results\OrientationDependency\20210927_LogFile_OrientationDependency_Lipid_H_500ns_1ps_nH40.txt')
 correlationFunction = (ifft(fft(sphericalHarmonic,zeroPaddingLength,2) ... 
     .*fft(conj(sphericalHarmonic),zeroPaddingLength,2),[],2)/timeSteps);
-logMemoryUsage('C:\Users\maxoe\Google Drive\Promotion\Results\OrientationDependency\20210927_LogFile_OrientationDependency_Lipid_H_500ns_1ps_nH40.txt')
 correlationFunction = sum(correlationFunction(:,(1:numLags)),1);
 
-logMemoryUsage('C:\Users\maxoe\Google Drive\Promotion\Results\OrientationDependency\20210927_LogFile_OrientationDependency_Lipid_H_500ns_1ps_nH40.txt')
 end
 
 
