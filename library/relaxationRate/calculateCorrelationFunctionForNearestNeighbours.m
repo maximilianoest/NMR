d5ee1aba848ce90ec32f6a9bfad0dcbe302b3c81
Maximilian Oesterreicher <maxoeschi@outlook.de>
2021-09-30 09:35:08 +0200

    fixed bug in nearest neighbour analysis

--- library/relaxationRate/calculateCorrelationFunctionForNearestNeighbours.m
+++ library/relaxationRate/calculateCorrelationFunctionForNearestNeighbours.m
@@ -7,7 +7,7 @@ zeroPaddingLength = 2^(nextpow2(timeSteps)+1);
 
 correlationFunction = ifft(fft(sphericalHarmonic,zeroPaddingLength,2) ... 
     .*conj(fft(sphericalHarmonic,zeroPaddingLength,2)),[],2)/timeSteps;
-correlationFunction = sum(correlationFunction(:,1:numLags),1);
+correlationFunction = correlationFunction(:,1:numLags);
 
 end
 
