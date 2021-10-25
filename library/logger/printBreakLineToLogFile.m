b08767506d986d01afe67ad05fcdd9c55870edff
Maximilian Oesterreicher <maxoeschi@outlook.de>
2021-10-25 16:08:12 +0200

    add base code for analysing sampling rate and fixed some logger details

--- library/logger/printBreakLineToLogFile.m
+++ library/logger/printBreakLineToLogFile.m
@@ -1,9 +1,6 @@
 function printBreakLineToLogFile(path2LogFile)
 
-fileId = fopen(path2LogFile,'a');
-if fileId == -1
-    error('Cannot open log file.')
-end
-fprintf(fileId,'____________________________________________________\n');
-fclose(fileId);
+logMessage('____________________________________________________' ...
+    ,path2LogFile,false);
+
 end
