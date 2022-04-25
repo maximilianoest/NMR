4ac916cbfcacf4c67edabee2516189de28ccfa83
maximilianoest <50007913+maximilianoest@users.noreply.github.com>
2022-04-25 11:56:37 +0200

    function to determine how many atoms needed

--- CorrelationFunctions/reconstructR1WithCorrFunc.m
+++ CorrelationFunctions/reconstructR1WithCorrFunc.m
@@ -2,30 +2,30 @@ clc
 clear all
 close all
 
-results = load("C:\Users\maxoe\Google Drive\Promotion\Results\relevantNearestNeighboursCorrelationFunction\20220422_Results_relevantNearestNeighboursCorrelationFunction_DOPSlipid4.mat");
+results = load("C:\Users\maxoe\Google Drive\Promotion\Results\LIPIDS\PLPC\necessaryNearestNeighbours\20220419_Results_relevantNearestNeighbours_PLPClipid.mat");
 baseConfiguration = readConfigurationFile('../baseConfiguration.txt');
 addpath(genpath(baseConfiguration.path2LibraryOnLocalMachine));
 
 orientationAngles = results.orientationAngles;
 positionAngles = results.positionAngles;
 
-disp('-- Only summed up correlation function --')
-corrFunc1W0 = results.sumCorrelationFunction1W0Saver/results.atomCounter;
-corrFunc2W0 = results.sumCorrelationFunction2W0Saver/results.atomCounter;
-
-for orientationNr = 1:length(orientationAngles)
-    for positionNr = 1:length(positionAngles)
-        [spectralDensity1W0, spectralDensity2W0] = ...
-            calculateSpectralDensities(squeeze(corrFunc1W0(orientationNr ...
-            ,positionNr,:))',squeeze(corrFunc2W0(orientationNr,positionNr,:))' ...
-            ,results.omega0,results.samplingFrequency,results.lags);
-        r1WithPerturbationTheory(orientationNr,positionNr) = ...
-            calculateR1WithSpectralDensity(spectralDensity1W0 ...
-            ,spectralDensity2W0,results.dipolDipolConstant);
-    end
-end
-r1WithPerturbationTheory
-r1 = mean(results.r1WithPerturbationTheory(:,:,:,1),3)
+% disp('-- Only summed up correlation function --')
+% corrFunc1W0 = results.sumCorrelationFunction1W0Saver/results.atomCounter;
+% corrFunc2W0 = results.sumCorrelationFunction2W0Saver/results.atomCounter;
+% 
+% for orientationNr = 1:length(orientationAngles)
+%     for positionNr = 1:length(positionAngles)
+%         [spectralDensity1W0, spectralDensity2W0] = ...
+%             calculateSpectralDensities(squeeze(corrFunc1W0(orientationNr ...
+%             ,positionNr,:))',squeeze(corrFunc2W0(orientationNr,positionNr,:))' ...
+%             ,results.omega0,results.samplingFrequency,results.lags);
+%         r1WithPerturbationTheory(orientationNr,positionNr) = ...
+%             calculateR1WithSpectralDensity(spectralDensity1W0 ...
+%             ,spectralDensity2W0,results.dipolDipolConstant);
+%     end
+% end
+% r1WithPerturbationTheory
+% r1 = mean(results.r1WithPerturbationTheory(:,:,:,1),3)
 
 corrFunc1W0 = results.correlationFunction1W0Saver;
 corrFunc2W0 = results.correlationFunction2W0Saver;
@@ -44,30 +44,30 @@ for orientationNr = 1:length(orientationAngles)
     end
 end
 r1WithPerturbationTheory
-r1 = mean(results.r1WithPerturbationTheory(:,:,:,1),3)
+r1 = mean(results.r1WithPerturbationTheory(:,:,:),3)
 
 
-corrFunc1W0 = results.allCorrelationFunction1W0Saver;
-corrFunc2W0 = results.allCorrelationFunction2W0Saver;
-
-disp('-- First caclulate R1 then average R1 --')
-for atomCounter = 1:results.atomCounter
-    for orientationNr = 1:length(orientationAngles)
-        for positionNr = 1:length(positionAngles)
-            
-            [spectralDensity1W0, spectralDensity2W0] = ...
-                calculateSpectralDensities(squeeze(corrFunc1W0(atomCounter,orientationNr ...
-                ,positionNr,:))',squeeze(corrFunc2W0(atomCounter,orientationNr,positionNr,:))' ...
-                ,results.omega0,results.samplingFrequency,results.lags);
-            r1WithPerturbationTheory(atomCounter,orientationNr,positionNr) = ...
-                calculateR1WithSpectralDensity(spectralDensity1W0 ...
-                ,spectralDensity2W0,results.dipolDipolConstant);
-        end
-    end
-end
-
-squeeze(mean(r1WithPerturbationTheory,1))
-r1 = mean(results.r1WithPerturbationTheory(:,:,:,1),3)
+% corrFunc1W0 = results.allCorrelationFunction1W0Saver;
+% corrFunc2W0 = results.allCorrelationFunction2W0Saver;
+% 
+% disp('-- First caclulate R1 then average R1 --')
+% for atomCounter = 1:results.atomCounter
+%     for orientationNr = 1:length(orientationAngles)
+%         for positionNr = 1:length(positionAngles)
+%             
+%             [spectralDensity1W0, spectralDensity2W0] = ...
+%                 calculateSpectralDensities(squeeze(corrFunc1W0(atomCounter,orientationNr ...
+%                 ,positionNr,:))',squeeze(corrFunc2W0(atomCounter,orientationNr,positionNr,:))' ...
+%                 ,results.omega0,results.samplingFrequency,results.lags);
+%             r1WithPerturbationTheory(atomCounter,orientationNr,positionNr) = ...
+%                 calculateR1WithSpectralDensity(spectralDensity1W0 ...
+%                 ,spectralDensity2W0,results.dipolDipolConstant);
+%         end
+%     end
+% end
+% 
+% squeeze(mean(r1WithPerturbationTheory,1))
+% r1 = mean(results.r1WithPerturbationTheory(:,:,:,1),3)
 
 
 
---
+++ atomsToCalculate/areNumberOfAtomsEnough.m
@@ -0,0 +1,57 @@
+clc; clear all; close all;
+
+results = load("C:\Users\maxoe\Google Drive\Promotion\Results\LIPIDS\PLPC\necessaryNearestNeighbours\20220419_Results_relevantNearestNeighbours_PLPClipid.mat");
+
+r1 = results.r1WithPerturbationTheory;
+saving = 1;
+savingPath = sprintf(['C:\\Users\\maxoe\\Google Drive\\Promotion' ...
+    '\\Zwischenergebnisse\\%s_numberOfAtoms_%s\\'] ...
+    ,datestr(date,'yyyymmdd'),results.whichLipid);
+if ~exist(savingPath,'dir')
+    mkdir(savingPath);
+end
+
+
+orientationAngles = results.orientationAngles;
+positionAngles = results.positionAngles;
+
+atomNrStepsSize = 10;
+atomCounter = round(results.atomCounter/atomNrStepsSize);
+averageR1 = zeros(1,atomCounter);
+stdR1 = zeros(1,atomCounter);
+numberOfAtomsAxis = 1:atomNrStepsSize:results.atomCounter;
+
+figurePosAndSize = [50 50 900 600];
+figure('Position',figurePosAndSize);
+set(gcf,'DefaultLineLineWidth',1.5)
+set(gca,'FontSize',16)
+
+legendEntries = {};
+hold on
+for orientationNr = 1:length(orientationAngles)
+    for positionNr = 1:length(positionAngles)
+       r1ForOriAndPos = squeeze(r1(orientationNr,positionNr,:,1));
+        for atomNr = 1:atomCounter
+           averageR1(atomNr) = mean( ...
+               r1ForOriAndPos(1:(atomNr-1)*atomNrStepsSize+1));
+           stdR1(atomNr) = std(...
+               r1ForOriAndPos(1:(atomNr-1)*atomNrStepsSize+1));
+        end 
+       errorbar(numberOfAtomsAxis,averageR1,stdR1);
+       legendEntries{end+1} = sprintf("$\\theta$: %d, $\\varphi$: %d" ...
+           ,rad2deg(orientationAngles(orientationNr)) ...
+           ,rad2deg(positionAngles(positionNr))); %#ok<SAGROW>
+    end
+end
+hold off
+legend(legendEntries,'Interpreter','latex');
+xlabel('Number of calculated atoms');
+ylabel('Relaxation Rate with STD [Hz]');
+title('Relaxation rate in dependence of calculated atoms')
+grid minor
+
+if saving
+    savingName = sprintf('r1DependentOnCalculatedAtoms_%s' ...
+        ,results.whichLipid); %#ok<UNRCH>
+    print(gcf,[savingPath savingName],'-dpng','-r300');
+end
